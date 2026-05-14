bc87a1ea151bd58d466402b20044e227a8e814b8
axxuetam <axxuetam@gmail.com>
2026-05-14 18:41:05 +0500

    Разработанный классы общего-пользования. Kilunina

---
+++ TaskManager_Kilunina/Classes/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager_Kilunina.Classes
+{
+    public class Notification : INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler PropertyChanging;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanging != null)
+                PropertyChanging(this, new PropertyChangingEventArgs(prop));
+        }
+    }
+}
