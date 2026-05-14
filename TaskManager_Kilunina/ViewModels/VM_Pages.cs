c0974669ea6dfe53b851de8ad6995a5ce1f83099
axxuetam <axxuetam@gmail.com>
2026-05-14 19:33:21 +0500

    Разработанная модель представления главного окна. Kilunina

--- TaskManager_Kilunina/ViewModels/VM_Pages.cs
+++ TaskManager_Kilunina/ViewModels/VM_Pages.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager_Kilunina.Classes;
 
 namespace TaskManager_Kilunina.ViewModels
 {
-    public class VM_Pages
+    public class VM_Pages : Notification
     {
-        public VM_Tasks vm_tasks;
+        public VM_Tasks vm_tasks = new VM_Tasks();
+        public VM_Pages() =>
+            MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
     }
 }
