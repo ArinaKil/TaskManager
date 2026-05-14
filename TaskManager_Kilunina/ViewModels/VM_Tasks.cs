e7c51291c6422234f67cc6f50b5c5b545dc191a1
axxuetam <axxuetam@gmail.com>
2026-05-14 19:40:02 +0500

    Разработанная модель представления задач. Kilunina

--- TaskManager_Kilunina/ViewModels/VM_Tasks.cs
+++ TaskManager_Kilunina/ViewModels/VM_Tasks.cs
@@ -5,12 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using TaskManager_Kilunina.Models;
 using TaskManager_Kilunina.Context;
+using TaskManager_Kilunina.Classes;
+using System.Collections.ObjectModel;
 
 namespace TaskManager_Kilunina.ViewModels
 {
-    public class VM_Tasks
+    public class VM_Tasks : Notification
     {
+        public ObservableCollection<Tasks> Tasks { get; set; }
         public TasksContext tasksContext;
-        public List<Tasks> Tasks = new List<Tasks>();
+
+        public VM_Tasks() =>
+            Tasks = new ObservableCollection<Tasks>(tasksContext.Tasks.OrderBy(x => x.Done));
+        public RealyCommand OnAddTask
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    Tasks newTask = new Tasks()
+                    {
+                        DateExecute = DateTime.Now
+                    };
+                    Tasks.Add(newTask);
+                    tasksContext.Tasks.Add(newTask);
+                    tasksContext.SaveChanges();
+                });
+            }
+        }
     }
 }
