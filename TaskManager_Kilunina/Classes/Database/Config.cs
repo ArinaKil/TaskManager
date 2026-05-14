f627e90011b06bdd695887f39e8d341cd06b63a6
axxuetam <axxuetam@gmail.com>
2026-05-14 18:21:11 +0500

    Созданный класс Config. Kilunina

---
+++ TaskManager_Kilunina/Classes/Database/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager_Kilunina.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "sever=127.0.0.1;uid=root;pwd=;database=TaskManager";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
